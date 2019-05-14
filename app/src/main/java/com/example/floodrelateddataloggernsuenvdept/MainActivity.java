640ce7f83437b355c286b0f5520930fc8ec585db
taoseef <taoseef.ishtiak@northsouth.edu>
2019-05-15 03:25:44 +0600

    added ui related ids in backend code

--- app/src/main/java/com/example/floodrelateddataloggernsuenvdept/MainActivity.java
+++ app/src/main/java/com/example/floodrelateddataloggernsuenvdept/MainActivity.java
@@ -25,17 +25,47 @@ import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText mashik_bey, mashik_ay, krishijomi_promian, krishijomi_promian_unit, boshot_bhitar_dhoron, gobadi_poshu_songkha_hash, gobadi_poshu_songkha_murgi, gobadi_poshu_songkha_goru, gobadi_poshu_songkha_chagol, pukure_mach_chash_kore_kina, mach_theke_bochore_koto_ay_kore,
-    boshot_bari_khoy_khoti_yesorno, boshot_bari_khoy_khotir_dhoron, boshot_bari_khoy_khotir_poriman, krishi_jomir_khoti_hoyeche_kina, krishi_jomir_ki_dhoroner_khoti, krishi_jomir_ki_poriman_khoti_takay, fosholer_khoti, fosholer_khotir_poriman_takay, pukure_macher_khoti, pukure_macher_khotir_poriman_takay,
-            onnanno_khotir_dhoron, onnanno_khotir_poriman_takay, arthik_koto_taka, shohayotar_dhoron, kar_kach_theke_asha_koren, onnanno_ki_dhoroner_shoyayota;
-    private RadioGroup radioKrishiJomiAcheKina, radioKrishiJomiPoriman, radioBoshotVitarDhoron, radioPukureMachChashKoreKina;
+    EditText mashik_bey, mashik_ay, krishijomi_promian, gobadi_poshu_songkha_hash, gobadi_poshu_songkha_murgi, gobadi_poshu_songkha_goru, gobadi_poshu_songkha_chagol, mach_theke_bochore_koto_ay_kore,
+    boshot_bari_khoy_khotir_poriman, krishi_jomir_ki_poriman_khoti_takay, fosholer_khotir_poriman_takay, pukure_macher_khotir_poriman_takay,
+            onnanno_khotir_dhoron, onnanno_khotir_poriman_takay, arthik_koto_taka;
+    private RadioGroup radioKrishiJomiAcheKina, radioKrishiJomiPoriman, radioBoshotVitarDhoron, radioPukureMachChashKoreKina,
+            radioBoshotBarirKhoti, radioKrishiJomiKhoti, radioFoshol, radioPukurerMach, radioEMuhurteShahajjo, radioSohayotarDhoron,
+            radioOnudanAsha, radioOnnannohole;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mashik_bey = findViewById(R.id.input1);
+        mashik_ay = findViewById(R.id.input2);
+        krishijomi_promian = findViewById(R.id.input3);
+        gobadi_poshu_songkha_hash = findViewById(R.id.input4);
+        gobadi_poshu_songkha_murgi = findViewById(R.id.input5);
+        gobadi_poshu_songkha_goru = findViewById(R.id.input6);
+        gobadi_poshu_songkha_chagol = findViewById(R.id.input7);
+        mach_theke_bochore_koto_ay_kore = findViewById(R.id.input8);
+        boshot_bari_khoy_khotir_poriman = findViewById(R.id.input9);
+        krishi_jomir_ki_poriman_khoti_takay = findViewById(R.id.input10);
+        fosholer_khotir_poriman_takay = findViewById(R.id.input11);
+        pukure_macher_khotir_poriman_takay = findViewById(R.id.input12);
+        onnanno_khotir_dhoron = findViewById(R.id.input13);
+        onnanno_khotir_poriman_takay = findViewById(R.id.input14);
+        arthik_koto_taka = findViewById(R.id.input15);
+
+        //radio button inputs
         radioKrishiJomiAcheKina = (RadioGroup) findViewById(R.id.radioKrishiJomiAcheKina);
+        radioKrishiJomiPoriman = (RadioGroup) findViewById(R.id.radioKrishiJomiPoriman);//unit
+        radioBoshotVitarDhoron = (RadioGroup) findViewById(R.id.radioBoshotVitarDhoron);
+        radioPukureMachChashKoreKina = (RadioGroup) findViewById(R.id.radioPukureMachChashKoreKina);
+        radioBoshotBarirKhoti = (RadioGroup) findViewById(R.id.radioBoshotBarirKhoti);
+        radioKrishiJomiKhoti = (RadioGroup) findViewById(R.id.radioKrishiJomiKhoti);
+        radioFoshol = (RadioGroup) findViewById(R.id.radioFoshol);
+        radioPukurerMach = (RadioGroup) findViewById(R.id.radioPukurerMach);
+        radioEMuhurteShahajjo = (RadioGroup) findViewById(R.id.radioEMuhurteShahajjo);
+        radioSohayotarDhoron = (RadioGroup) findViewById(R.id.radioSohayotarDhoron);
+        radioOnudanAsha = (RadioGroup) findViewById(R.id.radioOnudanAsha);
+        radioOnnannohole = (RadioGroup) findViewById(R.id.radioOnnannohole);
+
         //printKeyHash();
 
     }
@@ -52,6 +82,17 @@ public class MainActivity extends AppCompatActivity {
 
         // get selected radio button from radioGroup
         int selectedIdRG1 = radioKrishiJomiAcheKina.getCheckedRadioButtonId();
+        int selectedIdRG2 = radioKrishiJomiAcheKina.getCheckedRadioButtonId();
+        int selectedIdRG3 = radioKrishiJomiAcheKina.getCheckedRadioButtonId();
+        int selectedIdRG4 = radioKrishiJomiAcheKina.getCheckedRadioButtonId();
+        int selectedIdRG5 = radioKrishiJomiAcheKina.getCheckedRadioButtonId();
+        int selectedIdRG6 = radioKrishiJomiAcheKina.getCheckedRadioButtonId();
+        int selectedIdRG7 = radioKrishiJomiAcheKina.getCheckedRadioButtonId();
+        int selectedIdRG8 = radioKrishiJomiAcheKina.getCheckedRadioButtonId();
+        int selectedIdRG9 = radioKrishiJomiAcheKina.getCheckedRadioButtonId();
+        int selectedIdRG10 = radioKrishiJomiAcheKina.getCheckedRadioButtonId();
+        int selectedIdRG11 = radioKrishiJomiAcheKina.getCheckedRadioButtonId();
+        int selectedIdRG12 = radioKrishiJomiAcheKina.getCheckedRadioButtonId();
 
         // find the radiobutton by returned id
         RadioButton krishijomi_ache_kina = (RadioButton) findViewById(selectedIdRG1);
