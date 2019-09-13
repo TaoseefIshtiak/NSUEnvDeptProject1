ab185ad0cc249a917f6dad19a92b3314b53467bc
taoseef <taoseef.ishtiak@northsouth.edu>
2019-09-13 17:38:41 +0600

    ddd

---
+++ app/src/main/java/com/example/floodrelateddataloggernsuenvdept/MainActivity2.java
@@ -0,0 +1,31 @@
+package com.example.floodrelateddataloggernsuenvdept;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
